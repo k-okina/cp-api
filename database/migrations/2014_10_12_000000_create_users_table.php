97f0f60c5f1e9a2afa2fee16a5f42c388b34be02
k-okina <okinakahiro@gmail.com>
2016-11-18 17:09:13 +0900

    Update users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->unsignedBigInteger('facebook_id');
+            $table->unsignedBigInteger('twitter_id');
+            $table->unsignedBigInteger('instagram_id');
+            $table->unsignedBigInteger('tumblr_id');
             $table->rememberToken();
             $table->timestamps();
         });
